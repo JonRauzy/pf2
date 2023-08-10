47af80988a9acebd63fd9c0eb0def740c076020b
JonRauzy <jonathan.rauzy@gmail.com>
2023-08-10 10:50:14 +0200

    tailwind almost works

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
 @extends('base')
+
+@section('pageTitle', "Jon Rauzy")
+
+@section('content')
+
+    <h1 class="text-3xl font-bold">LALALALALALAA</h1>
+    
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/base.blade.php",
+    "./resources/js/app.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
