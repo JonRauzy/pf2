0e18cdc0814d26cf5d5a6e6f1e9de1d2dfb76e08
JonRauzy <jonathan.rauzy@gmail.com>
2023-08-10 13:08:10 +0200

    typo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 
-<div class="font-bold">
+<div>
     <h1 class="text-3xl font-bold">LALALALALALAA</h1>
-    <p class="font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum amet culpa nesciunt sint nam harum, deleniti enim molestias? Similique quos numquam amet? Quis blanditiis quos, rerum repellendus accusantium non neque!</p>
+    <p class="p-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum amet culpa nesciunt sint nam harum, deleniti enim molestias? Similique quos numquam amet? Quis blanditiis quos, rerum repellendus accusantium non neque!</p>
 </div>
 @endsection
